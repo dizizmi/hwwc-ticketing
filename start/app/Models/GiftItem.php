3481ed8b168cd7af278e118e7041b4351e87a93d
dizizmi <65617120+dizizmi@users.noreply.github.com>
2026-03-03 18:32:17 +0800

    protected $fillable ensure no malicious user from update sensi data

---
+++ start/app/Models/GiftItem.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GiftItem extends Model
+{
+    protected $fillable = [
+        'name',
+        'size',
+        'quantity',
+        'redeemed'
+    ];
+
+    
+}
