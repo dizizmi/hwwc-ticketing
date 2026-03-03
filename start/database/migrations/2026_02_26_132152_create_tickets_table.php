b3605bd26adba029657b4ef2dd49f2e97375aca5
dizizmi <65617120+dizizmi@users.noreply.github.com>
2026-03-03 15:39:33 +0800

    ticket migration file

---
+++ start/database/migrations/2026_02_26_132152_create_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('ticket_code')->unique();
+            $table->foreignId('guest_id')->constrained()->onDelete('cascade');
+            $table->boolean('is_active')->default(true);
+        
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
